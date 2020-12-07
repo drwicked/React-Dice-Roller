f1b6633737724c06aacfaf0d2a570054fe528957
Jeff P <yeffrey@gmail.com>
2020-12-06 22:28:07 -0600

    updates

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
 import React from 'react';
-import Navbar from './Navbar'
+import styled from 'styled-components';
 import Die from './Die'
 import './App.css'
+
+const DiceWrap = styled.div`
+  padding: 20px;
+`;
+
+const prompts = [
+  'Fire Breaks Out',
+  'Lost Item Found',
+  'Creature Attack',
+  'Violent Weather',
+  'Betrayal Revealed',
+  'Rival Returns',
+];
 
 class App extends React.Component {
   render() {
     return (
-        <div>
-            <Navbar />
-            <main>
-                <Die />
-            </main>
-        </div>
+      <div>
+        <main>
+          <DiceWrap><Die sides={prompts} id="diceA" /></DiceWrap>
+          <DiceWrap><Die sides={prompts} id="diceB" /></DiceWrap>
+          <DiceWrap><Die sides={prompts} id="diceC" /></DiceWrap>
+        </main>
+      </div>
     );
   }
 }
--- src/Die.js
+++ src/Die.js
@@ -2,46 +2,55 @@ import React from 'react';
 import './Die.css'
 
 class Die extends React.Component {
-    constructor(props) {
-        super(props);
-        this.sides = [
-            "translateZ(-100px) rotateY(0deg)",
-            "translateZ(-100px) rotateX(-180deg)",
-            "translateZ(-100px) rotateY(-90deg)",
-            "translateZ(-100px) rotateY(90deg)",
-            "translateZ(-100px) rotateX(-90deg)",
-            "translateZ(-100px) rotateX(90deg)"
-        ];
-        this.handleClick = this.handleClick.bind(this);
-        this.render = this.render.bind(this);
-    }
+  constructor(props) {
+      super(props);
+      this.sides = [
+          "translateZ(-100px) rotateY(0deg)",
+          "translateZ(-100px) rotateX(-180deg)",
+          "translateZ(-100px) rotateY(-90deg)",
+          "translateZ(-100px) rotateY(90deg)",
+          "translateZ(-100px) rotateX(-90deg)",
+          "translateZ(-100px) rotateX(90deg)"
+      ];
+      this.handleClick = this.handleClick.bind(this);
+      this.render = this.render.bind(this);
+      this.state = {
+        rolling: false,
+      }
+  }
 
-    handleClick(e) {
-        var die = document.getElementById("die");
-        var sides = this.sides
+  handleClick(e) {
+    const { id } = this.props;
+    console.log('id', id);
+    var die = e.target.parentNode;
+    console.log('e.target', e.target.parentNode);
+    console.log('die', die);
+    var sides = this.sides
 
-        die.classList.add("rolling");
+    this.setState({ rolling: true });
 
-        setTimeout(function () {
-            var roll = Math.floor(Math.random() * (sides.length))
-            die.classList.remove("rolling");
-            die.style.transform = sides[roll];
-        }, 750);
-    }
+    setTimeout(() => {
+      var roll = Math.floor(Math.random() * (sides.length))
+      this.setState({ rolling: false });
+      die.style.transform = sides[roll];
+    }, 750);
+  }
 
-    render() {
-        let divs = this.sides.map((side, index) => {
-            return <div className="side">{index+1}</div>
-        });
+  render() {
+    const { id, sides } = this.props;
+    const { rolling } = this.state;
+    let divs = this.sides.map((side, index) => {
+      return <div key={`side${index}`} className="side">{sides[index]}</div>
+    });
 
-        return (
-            <div className="die-container" onClick={ this.handleClick }>
-                <div id="die" className={'d' + this.sides.length}>
-                    { divs }
-                </div>
-            </div>
-        );
-    }
+    return (
+      <div className="die-container" onClick={ this.handleClick }>
+        <div id={id} className={`theDice d${this.sides.length} ${rolling && 'rolling'}`}>
+          { divs }
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Die;
